2a94a549d6fe1a0b8857151111c679040e7f011d
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2024-05-02 15:36:58 +1200

    Add rapier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import { forwardRef, useState, useEffect } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
-import { Gltf, PerspectiveCamera, Bvh, Center } from "@react-three/drei";
+import { Gltf, PerspectiveCamera, Center } from "@react-three/drei";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 
-const Model = forwardRef(({ id, position, ...props }, ref) => {
+const Model = forwardRef(({ id, position, rotation, ...props }, ref) => {
   const three = useThree();
   return (
-    <Bvh>
-      <Center top position={position}>
-        <Gltf {...props} ref={ref} />
-      </Center>
-    </Bvh>
+    <RigidBody
+      colliders="trimesh"
+      userData={{ pieceData: { id } }}
+      transformState={false}
+      type="fixed"
+      position={position}
+      rotation={rotation}
+    >
+      <Gltf {...props} ref={ref} />
+    </RigidBody>
   );
 });
 
+const initialDuckPos = [0, 3, 0];
 function App() {
-  const [duckPos, setDuckPos] = useState([0, 5, 0]);
-  const [shell1Pos, setShell1Pos] = useState([0, 1, 0]);
+  const [duckPos, setDuckPos] = useState(initialDuckPos);
+  const [shell1Pos, setShell1Pos] = useState([0, 0, 0]);
 
   const handleGravity = () => {
-    const newY = 2.5; // to be calculated
+    const newY = 0; // to be calculated
     setDuckPos([0, newY, 0]);
   };
 
   // Helper for visualising what is happening
   const run = () => {
-    setDuckPos([0, 5, 0]);
+    setDuckPos(initialDuckPos);
     const t = setTimeout(handleGravity, 300);
     return () => clearTimeout(t);
   };
@@ -33,30 +40,25 @@ function App() {
   return (
     <>
       <Canvas style={{ height: "100vh", width: "100vw" }} onClick={run}>
-        <PerspectiveCamera
-          makeDefault
-          position={[0, 3, 10]}
-          rotation={[0, 0, 0]}
-        />
-        <ambientLight intensity={2} />
+        <Physics debug>
+          <PerspectiveCamera
+            makeDefault
+            position={[0, 0.2, 10]}
+            rotation={[0, 0, 0]}
+          />
+          <ambientLight intensity={2} />
 
-        <gridHelper />
+          <gridHelper />
 
-        <Model src="Duck.glb" id="duck" position={duckPos} />
-        <Model
-          src="IridescenceAbalone.glb"
-          id="shell1"
-          position={shell1Pos}
-          scale={[20, 20, 20]}
-          rotation={[2.4, 0, 0]}
-        />
-
-        <Model
-          src="IridescenceAbalone.glb"
-          id="shell2"
-          position={[-5, 0, 0]}
-          scale={[20, 20, 20]}
-        />
+          <Model src="Duck.glb" id="duck" position={duckPos} />
+          <Model
+            src="IridescenceAbalone.glb"
+            id="shell1"
+            position={shell1Pos}
+            scale={[20, 20, 20]}
+            rotation={[0, 0, 0]}
+          />
+        </Physics>
       </Canvas>
     </>
   );
