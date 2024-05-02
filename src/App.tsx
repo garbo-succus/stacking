1ea503119eaa81c5fc6775dcce3ea8240305263f
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2024-05-02 18:21:06 +1200

    Add castShapeDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import { forwardRef, useState, useEffect } from "react";
+import { forwardRef, useRef, useState, useEffect } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { Gltf, PerspectiveCamera, Center } from "@react-three/drei";
-import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import {
+  useRapier,
+  Physics,
+  RigidBody,
+  CuboidCollider,
+} from "@react-three/rapier";
+import { Quaternion, Euler } from "three";
+
+// Reusuable objects to save on GC
+const quaternion = new Quaternion();
+const euler = new Euler();
+
+// Cast downward ray from origin & get list of pieces (sorted by Y position)
+export const castShapeDown = (
+  { world },
+  { shape, position, rotation, distance = 10000 },
+  predicate,
+) =>
+  world.castShape(
+    { x: position[0], y: distance, z: position[2] },
+    quaternion.setFromEuler(euler.set(...rotation)),
+    { x: 0, y: -1, z: 0 },
+    shape,
+    distance + 1, // TODO: is it necessary to do +1?
+    false, // firstHitOnly
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    predicate,
+  );
 
 const Model = forwardRef(({ id, position, rotation, ...props }, ref) => {
   const three = useThree();
@@ -19,13 +49,33 @@ const Model = forwardRef(({ id, position, rotation, ...props }, ref) => {
   );
 });
 
-const initialDuckPos = [0, 3, 0];
-function App() {
+const initialDuckPos = [0, 3.5, 0];
+function World() {
   const [duckPos, setDuckPos] = useState(initialDuckPos);
-  const [shell1Pos, setShell1Pos] = useState([0, 0, 0]);
+  const [shell1Pos, setShell1Pos] = useState([0, 1.5, 0]);
+
+  const rapier = useRapier();
+  const duckRef = useRef();
 
   const handleGravity = () => {
-    const newY = 0; // to be calculated
+    const hit = castShapeDown(
+      rapier,
+      {
+        shape: new rapier.rapier.Cuboid(1, 1, 1), // TODO: duck geometry
+        position: [0, 0, 0],
+        rotation: [0, 0, 0],
+      },
+      (match) => {
+        const pieceData = rapier.rigidBodyStates.get(match.handle)?.object
+          ?.userData?.pieceData;
+        if (!pieceData) return false;
+        const { id } = pieceData;
+        if (id === "duck") return false;
+        return true;
+      },
+    );
+    console.log("castShapeDown", hit);
+    const newY = 10000 - hit.toi; // not working correctly
     setDuckPos([0, newY, 0]);
   };
 
@@ -38,8 +88,23 @@ function App() {
   useEffect(run, []);
 
   return (
+    <group onClick={run}>
+      <Model src="Duck.glb" id="duck" position={duckPos} ref={duckRef} />
+      <Model
+        src="IridescenceAbalone.glb"
+        id="shell1"
+        position={shell1Pos}
+        scale={[20, 20, 20]}
+        rotation={[0, 0, 0]}
+      />
+    </group>
+  );
+}
+
+function App() {
+  return (
     <>
-      <Canvas style={{ height: "100vh", width: "100vw" }} onClick={run}>
+      <Canvas style={{ height: "100vh", width: "100vw" }}>
         <Physics debug>
           <PerspectiveCamera
             makeDefault
@@ -47,17 +112,8 @@ function App() {
             rotation={[0, 0, 0]}
           />
           <ambientLight intensity={2} />
-
           <gridHelper />
-
-          <Model src="Duck.glb" id="duck" position={duckPos} />
-          <Model
-            src="IridescenceAbalone.glb"
-            id="shell1"
-            position={shell1Pos}
-            scale={[20, 20, 20]}
-            rotation={[0, 0, 0]}
-          />
+          <World />
         </Physics>
       </Canvas>
     </>
