f4f51447083937dce2dffdbd60f4c03673aa1d6e
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2024-05-08 15:45:33 +1200

    Update shapeCast

--- README.md
+++ README.md
@@ -11,27 +11,25 @@ We will trigger a function to perform a shapecast query, then use the results to
 The function should do a downward shapecast from a specified X/Z position, using the duck model. Ideally the origin Y position would be `Infinity`, but using a number like 1000 is acceptable.
 
 ```js
-const three = useThree()
+const three = useThree();
 
 const shapecastDown = ({
   origin: [
     x,
     y = Infinity, // this is preferable but 1000 is ok
-    z
+    z,
   ],
-  shape
+  shape,
 }) => {
-
   return {
     object, // what object we landed on
-    yOffset // Y position of duck
-  }
-}
+    yOffset, // Y position of duck
+  };
+};
 ```
 
 The function will return a reference or ID for the first model that the shapecast contacts, and the position of the contact.
 
-* We are centering models using [X, Y + height/2, Z] (see blue dots).
-* Models will have arbritrary rotations applied.
-* We will position the duck relative to the shell position, not world coordinates, however this just needs to be possible, and does not need to be implemented.
-
+- We are centering models using [X, Y + height/2, Z] (see blue dots).
+- Models will have arbritrary rotations applied.
+- We will position the duck relative to the shell position, not world coordinates, however this just needs to be possible, and does not need to be implemented.
--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,22 @@ import {
 } from "@react-three/rapier";
 import { Quaternion, Euler } from "three";
 
+import { create } from 'zustand'
+
 // Reusuable objects to save on GC
 const quaternion = new Quaternion();
 const euler = new Euler();
+
+// TODO: We shouldn't need to iterate over the whole list every time
+// Get a map of { [id]: rapierShape, ... }
+const getShapes = ({ colliderStates }) => {
+  const shapes = {};
+  colliderStates.forEach((state) => {
+    const key = state.worldParent?.userData?.pieceData?.id;
+    if (key) shapes[key] = state.collider._shape;
+  });
+  return shapes;
+};
 
 // Cast downward ray from origin & get list of pieces (sorted by Y position)
 export const castShapeDown = (
@@ -24,8 +37,8 @@ export const castShapeDown = (
     quaternion.setFromEuler(euler.set(...rotation)),
     { x: 0, y: -1, z: 0 },
     shape,
-    distance + 1, // TODO: is it necessary to do +1?
-    false, // firstHitOnly
+    distance,
+    false,
     undefined,
     undefined,
     undefined,
@@ -33,8 +46,9 @@ export const castShapeDown = (
     predicate,
   );
 
-const Model = forwardRef(({ id, position, rotation, ...props }, ref) => {
+const Model = ({ id, position, rotation, ...props }) => {
   const three = useThree();
+  const ref = useRef(); // TODO: Can we keep refs in a state object, instead of using getShapes?
   return (
     <RigidBody
       colliders="trimesh"
@@ -43,25 +57,33 @@ const Model = forwardRef(({ id, position, rotation, ...props }, ref) => {
       type="fixed"
       position={position}
       rotation={rotation}
+      ref={ref}
     >
-      <Gltf {...props} ref={ref} />
+      <Gltf {...props} />
     </RigidBody>
   );
-});
+};
 
 const initialDuckPos = [0, 3.5, 0];
 function World() {
   const [duckPos, setDuckPos] = useState(initialDuckPos);
-  const [shell1Pos, setShell1Pos] = useState([0, 1.5, 0]);
+  const [shell1Pos, setShell1Pos] = useState([0, 0, 0]);
 
   const rapier = useRapier();
-  const duckRef = useRef();
 
-  const handleGravity = () => {
+  const handleGravity = (id) => {
+    // if (!duckRef.current) return;
+    // const handle = duckRef.current.handle
+    // const shape = rapier.colliderStates.get(handle).collider._shape
+    // const shape = duckRef.current.collider()._shape;
+
+    const shapes = getShapes(rapier);
+    const shape = shapes[id];
+
     const hit = castShapeDown(
       rapier,
       {
-        shape: new rapier.rapier.Cuboid(1, 1, 1), // TODO: duck geometry
+        shape,
         position: [0, 0, 0],
         rotation: [0, 0, 0],
       },
@@ -69,33 +91,31 @@ function World() {
         const pieceData = rapier.rigidBodyStates.get(match.handle)?.object
           ?.userData?.pieceData;
         if (!pieceData) return false;
-        const { id } = pieceData;
-        if (id === "duck") return false;
+        if (pieceData.id === id) return false; // ignore selected pieces
         return true;
       },
     );
-    console.log("castShapeDown", hit);
-    const newY = 10000 - hit.toi; // not working correctly
+    const newY = hit ? 10000 - hit.toi : 0; // TODO: if no hit, run a query against a plane at Y=0
     setDuckPos([0, newY, 0]);
   };
 
   // Helper for visualising what is happening
   const run = () => {
     setDuckPos(initialDuckPos);
-    const t = setTimeout(handleGravity, 300);
+    const t = setTimeout(() => handleGravity("duck"), 100);
     return () => clearTimeout(t);
   };
   useEffect(run, []);
 
   return (
     <group onClick={run}>
-      <Model src="Duck.glb" id="duck" position={duckPos} ref={duckRef} />
+      <Model src="Duck.glb" id="duck" position={duckPos} />
       <Model
         src="IridescenceAbalone.glb"
         id="shell1"
         position={shell1Pos}
         scale={[20, 20, 20]}
-        rotation={[0, 0, 0]}
+        rotation={[2, 0, 0]}
       />
     </group>
   );
@@ -108,7 +128,7 @@ function App() {
         <Physics debug>
           <PerspectiveCamera
             makeDefault
-            position={[0, 0.2, 10]}
+            position={[0, 1, 10]}
             rotation={[0, 0, 0]}
           />
           <ambientLight intensity={2} />
