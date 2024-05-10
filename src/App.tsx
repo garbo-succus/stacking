4217b20e636b7b31b13c43a47bef5c4babbeced4
Bela Bohlender <bela@bohlender.email>
2024-05-10 19:48:00 +0200

    set origin to center bottom on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState, useEffect, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Gltf, PerspectiveCamera, OrbitControls } from "@react-three/drei";
-import { useRapier, Physics, RigidBody } from "@react-three/rapier";
+import { useState, useEffect, Suspense, forwardRef } from "react";
+import { Canvas, GroupProps } from "@react-three/fiber";
+import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import {
+  useRapier,
+  Physics,
+  RigidBody,
+  RapierContext,
+  Vector3Tuple,
+} from "@react-three/rapier";
 import { Quaternion, Euler, Object3D } from "three";
 import { getShape, setRigidBody } from "./rigidBodyMap";
-import { Vector3 } from "three";
-import { Box3 } from "three/src/Three.js";
+import { Box3, Group } from "three/src/Three.js";
+import { suspend } from "suspend-react";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 // Reusuable objects to save on GC
 const quaternion = new Quaternion();
@@ -13,13 +20,14 @@ const euler = new Euler();
 
 // Cast downward ray from origin & get list of pieces (sorted by Y position)
 export const castShapeDown = (
-  { world },
+  { world }: RapierContext,
   { shape, position, rotation, distance = 10000 },
   predicate
-) =>
-  world.castShape(
+): number => {
+  quaternion.setFromEuler(euler.set(...rotation));
+  const hit = world.castShape(
     { x: position[0], y: distance, z: position[2] },
-    quaternion.setFromEuler(euler.set(...rotation)),
+    quaternion,
     { x: 0, y: -1, z: 0 },
     shape,
     distance,
@@ -30,63 +38,62 @@ export const castShapeDown = (
     undefined,
     predicate
   );
+  // TODO: if no hit, run a query against a plane at Y=0
+  const newY = hit ? distance - hit.toi : 0;
+  return newY;
+};
 
+const loader = new GLTFLoader();
 const boxHelper = new Box3();
-const vectorHelper = new Vector3();
-const parentHelper = new Object3D();
 
-const Model = ({ id, position, rotation, center = false, ...props }) => {
-  const gltfRef = useRef<Object3D>(null);
-  const [offset, setOffset] = useState(new Vector3());
-  const offsetRef = useRef(offset);
+async function loadCenteredGltf(
+  _: any,
+  src: string,
+  ...rotation: Vector3Tuple
+) {
+  const { scene } = await loader.loadAsync(src);
+  const wrapper = new Object3D();
+  wrapper.add(scene);
+  wrapper.rotation.fromArray(rotation);
+  boxHelper.setFromObject(wrapper, true);
 
-  useEffect(() => {
-    if (!center || gltfRef.current == null) {
-      return;
-    }
+  //x & z center
+  boxHelper.getCenter(wrapper.position);
+  //y bottom
+  wrapper.position.y = boxHelper.min.y;
+  wrapper.position.multiplyScalar(-1);
+  console.log(wrapper.position.toArray());
+  return wrapper;
+}
 
-    //measuring the contents of the group
-    if (Array.isArray(rotation)) {
-      parentHelper.rotation.fromArray(rotation as any);
-    } else {
-      parentHelper.rotation.copy(rotation);
-    }
-    parentHelper.children[0] = gltfRef.current;
-    const parent = gltfRef.current.parent;
-    gltfRef.current.parent = parentHelper;
-    boxHelper.setFromObject(parentHelper, true);
-    gltfRef.current.parent = parent;
+const LoadCenteredGltfSymbol = Symbol("loadCenteredGltf");
 
-    //x & z center
-    boxHelper.getCenter(vectorHelper);
-    //y bottom
-    vectorHelper.y = boxHelper.min.y;
-    vectorHelper.multiplyScalar(-1);
-    if (offsetRef.current.distanceToSquared(vectorHelper) <= 0.001) {
-      return;
-    }
-    setOffset(vectorHelper);
-    offsetRef.current = vectorHelper;
-  }, [rotation]);
+const Gltf = forwardRef<
+  Group,
+  Omit<GroupProps, "rotation"> & { rotation: Vector3Tuple; src: string }
+>(({ src, rotation, ...props }, ref) => {
+  const gltf = suspend(loadCenteredGltf, [
+    LoadCenteredGltfSymbol,
+    src,
+    ...rotation,
+  ]);
+  return (
+    <group ref={ref} {...props}>
+      <primitive object={gltf} />
+    </group>
+  );
+});
 
-  const centeredPosition = new Vector3();
-  if (Array.isArray(position)) {
-    centeredPosition.fromArray(position);
-  } else {
-    centeredPosition.copy(position);
-  }
-  centeredPosition.add(offset);
-
+const Model = ({ id, position, rotation, ...props }) => {
   return (
     <RigidBody
       colliders="trimesh"
       userData={{ pieceData: { id } }}
       type="fixed"
-      position={centeredPosition}
-      rotation={rotation}
+      position={position}
       ref={setRigidBody.bind(null, id)}
     >
-      <Gltf ref={gltfRef} {...props} />
+      <Gltf rotation={rotation} {...props} />
     </RigidBody>
   );
 };
@@ -102,7 +109,7 @@ function World() {
     const shape = getShape(id);
     const position = duckPos;
     const rotation = duckRot;
-    const hit = castShapeDown(
+    const newY = castShapeDown(
       rapier,
       { shape, position, rotation },
       (match) => {
@@ -113,7 +120,6 @@ function World() {
         return true;
       }
     );
-    const newY = hit ? 10000 - hit.toi : 0; // TODO: if no hit, run a query against a plane at Y=0
     setDuckPos([0, newY, 0]);
   };
 
@@ -131,10 +137,9 @@ function World() {
       <Model
         src="IridescenceAbalone.glb"
         id="shell1"
-        center
         position={[0, 0, 0]}
         scale={[20, 20, 20]}
-        rotation={[2, 1, 0]}
+        rotation={[0, 1, 0]}
       />
     </group>
   );
